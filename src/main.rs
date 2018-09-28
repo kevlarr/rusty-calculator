ac5090cc4b49040cd1f153376ebdfc3e26608bd0
kevlarr <echo1+git@dvnl.net>
2018-09-28 17:00:19 -0400

    Get user input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::io;
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    println!("--Kevin's Quirky Calculator--\nEnter expression (eg. 124 + 12) or q to quit");
+
+    loop {
+        match get_input() {
+            ref input if input == "q" => std::process::exit(0),
+            ref input if input == "hi" => println!("Hello!"),
+            input => eval(input),
+        }
+    }
+}
+
+fn get_input() -> String {
+    print!("> ");
+    std::io::stdout().flush().unwrap();
+
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+
+    input.trim().to_string()
+}
+
+fn eval(input: String) {
+    println!("'{}'", input);
 }
