ab53cc0f7bda6c2325db41589cb7da1f7c0cbbb7
kevlarr <echo1+git@dvnl.net>
2018-09-28 17:40:46 -0400

    Parse left-hand arg and begin operator

--- src/main.rs
+++ src/main.rs
@@ -24,5 +24,42 @@ fn get_input() -> String {
 }
 
 fn eval(input: String) {
-    println!("'{}'", input);
+    let mut pieces = input.split_whitespace();
+
+    // Should be three pieces: left arg, operator, and right arg
+    let left_hand = match pieces.next() {
+        None => {
+            println!("Must provide left-hand argument");
+            return;
+        },
+        Some(s) => match isize::from_str_radix(s, 10) {
+            Ok(x) => x,
+            Err(_) => {
+                println!("Left-hand argument must be an integer");
+                return;
+            },
+        },
+    };
+
+    let operator = match pieces.next() {
+        None => {
+            println!("Must provide operator");
+            return;
+        },
+        Some(s) => match operator_from_str(s) {
+            Ok(op) => op,
+            Err(_) => {
+                println!("Must supply valid operator");
+                return;
+            },
+        },
+    };
+
+    println!("LH: {}", left_hand);
+    println!("Op: {}", operator);
+}
+
+fn operator_from_str(s: &str) -> Result<&str, &str> {
+    // FIXME
+    Ok(s)
 }
